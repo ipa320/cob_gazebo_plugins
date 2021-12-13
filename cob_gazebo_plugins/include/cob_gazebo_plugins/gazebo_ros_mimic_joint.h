69a258bea4acdb4601a32d08668e30b4ac7a5896
fmessmer <felixmessmer@gmail.com>
2021-12-13 13:21:45 +0100

    add MimicJoint gazebo plugin

---
+++ cob_gazebo_plugins/include/cob_gazebo_plugins/gazebo_ros_mimic_joint.h
@@ -0,0 +1,88 @@
+#ifndef GAZEBO_ROS_MIMIC_JOINT_HH
+#define GAZEBO_ROS_MIMIC_JOINT_HH
+
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Events.hh>
+#include <ros/ros.h>
+
+namespace gazebo
+{
+/** \defgroup MimicJoint XML Reference and Example
+
+\brief Ros Gazebo Ros MimicJoint Plugin.
+This model allow to mimic a joint without the use of the dynamics
+
+Example Usage:
+
+\verbatim
+<gazebo>
+  <plugin name="mimic_joint" filename="libgazebo_ros_mimic_plugin.so">
+    <jointName>joint_name</jointName>
+    <mimicJoint>mimic_joint_name</mimicJoint>
+    <multiplier>1.0</multiplier>
+    <offset>0.0</offset>
+    <pgain>1.0</pgain>
+  </plugin>
+</gazebo>
+\endverbatim
+\{
+*/
+
+
+/// \brief MimicJoint plugin
+/// This plugin allows to simulate mimic joints in gazebo
+class MimicJoint: public ModelPlugin
+{
+  /// \brief Constructor
+  /// \param parent The parent entity must be a Model
+  public: MimicJoint();
+
+  /// \brief Destructor
+  public: virtual ~MimicJoint();
+
+  /// \brief Load the controller
+  public: void Load( physics::ModelPtr _parent, sdf::ElementPtr _sdf );
+
+  /// \brief Update the controller
+  protected: virtual void UpdateChild();
+  
+  /// \brief multiplier
+  private: double multiplier_;
+
+  /// \brief offset
+  private: double offset_;
+
+  /// \brief proportionnal gain
+  private: double p_;
+
+  /// \brief A pointer to the Gazebo joint
+  private: physics::JointPtr joint_;
+  
+  /// \brief A pointer to the Gazebo joint
+  private: physics::JointPtr mimic_joint_;
+  
+  /// \brief A pointer to the Gazebo model
+  private: physics::ModelPtr model_;
+  
+  /// \brief A pointer to the Gazebo world
+  private: physics::WorldPtr world_;
+
+  /// \brief store bodyname
+  private: std::string joint_name_;
+
+  /// \brief store name of mimic joint
+  private: std::string mimic_joint_name_;
+
+  // Pointer to the update event connection
+  private: event::ConnectionPtr update_connection_;
+
+};
+
+/** \} */
+/// @}
+
+
+}
+
+#endif
---
+++ cob_gazebo_plugins/src/gazebo_ros_mimic_joint.cpp
@@ -0,0 +1,83 @@
+#include <cob_gazebo_plugins/gazebo_ros_mimic_joint.h>
+
+using namespace gazebo;
+
+GZ_REGISTER_MODEL_PLUGIN(MimicJoint);
+
+MimicJoint::MimicJoint()
+{
+  this->joint_.reset();
+  this->mimic_joint_.reset();
+}
+
+MimicJoint::~MimicJoint()
+{
+  //event::Events::DisconnectWorldUpdateBegin(this->update_connection_);
+}
+
+void MimicJoint::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
+{
+  this->model_ = _parent;
+  this->world_ = this->model_->GetWorld();
+
+
+  if (!_sdf->HasElement("jointName"))
+  {
+    ROS_FATAL("mimic_joint missing <jointName>, cannot proceed");
+    return;
+  }
+  else
+    this->joint_name_ = _sdf->GetElement("jointName")->Get<std::string>();
+
+  if (!_sdf->HasElement("mimicJoint"))
+  {
+    ROS_FATAL("mimic_joint missing <mimicJoint>, cannot proceed");
+    return;
+  }
+  else
+    this->mimic_joint_name_ = _sdf->GetElement("mimicJoint")->Get<std::string>();
+
+  if (!_sdf->HasElement("offset"))
+  {
+    ROS_INFO("mimic_joint missing <offset>, set default to 0.0");
+    this->offset_ = 0.0;
+  }
+  else
+    this->offset_ = _sdf->GetElement("offset")->Get<double>();
+
+  if (!_sdf->HasElement("multiplier"))
+  {
+    ROS_INFO("mimic_joint missing <multiplier>, set default to 1.0");
+    this->multiplier_ = 1.0;
+  }
+  else
+    this->multiplier_ = _sdf->GetElement("multiplier")->Get<double>();
+
+
+  // Get the name of the parent model
+  std::string modelName = _sdf->GetParent()->Get<std::string>("name");
+  gzdbg << "Plugin model name: " << modelName << "\n";
+
+  this->joint_ = model_->GetJoint(joint_name_);
+  this->mimic_joint_ = model_->GetJoint(mimic_joint_name_);
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&MimicJoint::UpdateChild, this));
+}
+
+void MimicJoint::UpdateChild()
+{
+#if GAZEBO_MAJOR_VERSION >= 8
+  const double desired_angle = this->mimic_joint_->Position(0)*this->multiplier_ + this->offset_;
+#else
+  const double desired_angle = this->mimic_joint_->GetAngle(0).Radian()*this->multiplier_ + this->offset_;
+#endif
+
+#if GAZEBO_MAJOR_VERSION >= 9
+  this->joint_->SetPosition(0, desired_angle, true);
+#else
+  this->joint_->SetPosition(0, desired_angle);
+#endif
+}
